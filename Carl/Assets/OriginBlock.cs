6b3c61bab5cccf894e9df1de053c13259153f4e6
pluka073 <71723381+pluka073@users.noreply.github.com>
2023-11-20 21:19:06 -0700

    testing .gitignore

---
+++ Carl/Assets/OriginBlock.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.EventSystems;
+using UnityEngine.EventSystems;
+using Palmmedia.ReportGenerator.Core;
+
+public class clickAndDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private GameObject PB;
+    private GameObject newPB;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        newPB = Instantiate(PB, canvas.transform);
+        newPB.SetActive(true);
+        newPB.GetComponent<ProgramBlock>().OnBeginDrag(eventData);
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        newPB.GetComponent<ProgramBlock>().OnDrag(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        newPB.GetComponent<ProgramBlock>().dropHandler();
+    }
+    
+}
+
+
+
